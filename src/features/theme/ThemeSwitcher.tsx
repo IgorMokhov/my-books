460bc5edfc716203412ec4bb083ab335fb7e81c6
Igor Mokhov <igormokhovid@gmail.com>
2024-01-09 04:35:03 +0300

    Added TypeScript types for theme features

--- src/features/theme/ThemeSwitcher.tsx
+++ src/features/theme/ThemeSwitcher.tsx
@@ -1,14 +1,16 @@
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 
-import { selectTheme, setTheme } from './theme-slice';
+import {setTheme } from './theme-slice';
+import { selectTheme } from './theme-selectors';
 
 import { IconButton } from '@mui/material';
 import LightModeIcon from '@mui/icons-material/LightMode';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
+import { useAppDispatch } from '../../redux-hooks';
 
 export const ThemeSwitcher = () => {
   const theme = useSelector(selectTheme);
-  const dispatch = useDispatch();
+  const dispatch = useAppDispatch();
 
   const onChangeTheme = () => {
     dispatch(setTheme(theme === 'light' ? 'dark' : 'light'));
